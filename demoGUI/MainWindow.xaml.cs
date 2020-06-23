5460382c6cdd775baf98dd7fd4d7aebeeef7b847
andrescass <andres.cass@gmail.com>
2020-06-23 02:03:09 -0300

    first release

--- demoGUI/MainWindow.xaml.cs
+++ demoGUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace demoGUI
 {
@@ -20,9 +21,29 @@ namespace demoGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            uploadBut.Background = Brushes.Green;
+            timer.Stop();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void uploadBut_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
